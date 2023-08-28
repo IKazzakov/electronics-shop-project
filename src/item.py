f922796715a69fa195e4304b2ec37aaced30fc21
Igor Kazakov <i.s.kazakov89@gmail.com>
2023-08-28 16:53:09 +0300

    add instantiate_from_csv, string_to_number, getter&setter fo name

--- src/item.py
+++ src/item.py
@@ -1,9 +1,14 @@
+import csv
+import os.path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    CSV_PATH = os.path.join('..', 'src', 'items.csv')  # Путь к файлу .csv
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,10 +18,38 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self):
+        """Геттер для атрибута name"""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """Сеттер для name, с проверкой длины наименования товара"""
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Класс-метод, инициализирующий экземпляры класса `Item`
+        данными из файла _src/items.csv"""
+        with open(cls.CSV_PATH, encoding='cp1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            cls.all.clear()
+            for row in reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string):
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(string))
 
     def calculate_total_price(self) -> float:
         """
